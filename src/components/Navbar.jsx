6367dc67ad4db88711eed19f4857a80f000bf345
codesclever <116460474+codesclever@users.noreply.github.com>
2022-10-29 00:18:22 +0530

    adding auto hide navbar in max-width:800px

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,46 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import "../css/Navbar.css";
 import { FaUserAlt } from "react-icons/fa";
-import {Link, useLocation, useNavigate} from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import authContext from "../context/auth/authContext";
 
 function Navbar(props) {
     const context = useContext(authContext);
-    const {loggedInUser} = context;
-    const [user,setUser] = useState();
+    const { loggedInUser } = context;
+    const [user, setUser] = useState();
     const navigate = useNavigate();
-
+    let isDoggle = false;
 
     const showNavBar = (e) => {
+        if(document.body.offsetWidth > 800){
+            return
+        }
+        
         const navbar = document.getElementById("navbar");
-        const bar = document.getElementById('bar');
+        const bar = document.getElementById("bar");
         if (navbar.style.display === "flex") {
             navbar.style.display = "none";
-            bar.innerHTML = '&#8801;'
+            bar.innerHTML = "&#8801;";
         } else {
             navbar.style.display = "flex";
-            bar.innerHTML = '&#10007;'
+            bar.innerHTML = "&#10007;";
         }
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         setUser(loggedInUser);
         // console.log("[+]",localStorage.getItem("auth-token"));
-    })
+    });
 
     let locate = useLocation().pathname;
     return (
-        
         <>
             <nav>
-                <div className="title" onClick={()=>{navigate('/')}}>
+                <div
+                    className="title"
+                    onClick={() => {
+                        navigate("/");
+                    }}>
                     <img
                         src={
                             process.env.PUBLIC_URL +
@@ -48,31 +55,66 @@ function Navbar(props) {
 
                 <ul id="navbar" className="navul">
                     <li className="navli">
-                        <Link to="/" className={locate==='/'?'active':''}>Home</Link>
+                        <Link
+                            to="/"
+                            className={locate === "/" ? "active" : ""}
+                            onClick={!isDoggle ? showNavBar : ""}>
+                            Home
+                        </Link>
                     </li>
                     {/* <li className="navli">
                         <Link to="/competition" className={locate==='/competition'?'active':''}>Competition</Link>
                     </li> */}
                     <li className="navli">
-                        <Link to="/leaderbord" className={locate==='/leaderbord'?'active':''}>Leaderbord</Link>
-                    </li>
-                    {<li className="navli">
-                        <Link to="/contact" className={locate==='/contact'?'active':''}>
-                            Contact
+                        <Link
+                            to="/leaderbord"
+                            className={locate === "/leaderbord" ? "active" : ""}
+                            onClick={!isDoggle ? showNavBar : ""}>
+                            Leaderbord
                         </Link>
-                    </li> }
+                    </li>
+                    {
+                        <li className="navli">
+                            <Link
+                                to="/contact"
+                                className={
+                                    locate === "/contact" ? "active" : ""
+                                }
+                                onClick={!isDoggle ? showNavBar : ""}>
+                                Contact
+                            </Link>
+                        </li>
+                    }
 
                     <li className="navli">
-                        <Link to="/about" className={locate==='/about'?'active':''}>About</Link>
+                        <Link
+                            to="/about"
+                            className={locate === "/about" ? "active" : ""}
+                            onClick={!isDoggle ? showNavBar : ""}>
+                            About
+                        </Link>
                     </li>
 
-                    <li className="login" >
-                        <Link to="/login" className={locate==='/login'?'active':''}>{!user && 'Login'}</Link>
-                        <Link to="/signin" className={locate==='/signin'?'active':''}>{!user && 'Sign up'}</Link>
-                        <Link to="/profile" className={locate==='/profile'?'active':''}>{user && <FaUserAlt size="20px" />}</Link>
+                    <li className="login">
+                        <Link
+                            to="/login"
+                            className={locate === "/login" ? "active" : ""}
+                            onClick={!isDoggle ? showNavBar : ""}>
+                            {!user && "Login"}
+                        </Link>
+                        <Link
+                            to="/signin"
+                            className={locate === "/signin" ? "active" : ""}
+                            onClick={!isDoggle ? showNavBar : ""}>
+                            {!user && "Sign up"}
+                        </Link>
+                        <Link
+                            to="/profile"
+                            className={locate === "/profile" ? "active" : ""}
+                            onClick={!isDoggle ? showNavBar : ""}>
+                            {user && <FaUserAlt size="20px" />}
+                        </Link>
                     </li>
-                    
-
                 </ul>
             </nav>
         </>
