14975c59f06584e8b56edc36fe890602a4892d43
codesclever <116460474+codesclever@users.noreply.github.com>
2022-10-28 23:25:11 +0530

    adding a not login user can visit the thankyou page

--- src/components/Thankyou.jsx
+++ src/components/Thankyou.jsx
@@ -1,12 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Tnc from './Tnc'
 import ReactGA from 'react-ga';
+import authContext from '../context/auth/authContext';
+import { useNavigate } from 'react-router-dom';
+
+
 ReactGA.initialize('G-4SPQWXL7YC');
 
 export default function Thankyou() {
+    const context = useContext(authContext);
+    const { loggedInUser } = context;
+    const navigate = useNavigate();
 
     useEffect(()=>{
+        if(!loggedInUser){
+            return navigate('/profile');
+        }
         ReactGA.pageview(window.location.pathname + window.location.search);
+        
     },[])
 
 
