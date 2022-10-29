425cfc85705071ab5092b5d7a4a98227aac8d918
codesclever <116460474+codesclever@users.noreply.github.com>
2022-10-29 19:19:57 +0530

    adding a promotoken

--- src/context/auth/signin.js
+++ src/context/auth/signin.js
@@ -1,18 +1,26 @@
-import config from '../../config';
+import config from "../../config";
 
-const signin =  async (props)=>{
+const signin = async (props) => {
     const host = config().HOST;
-    
-    const response = await fetch(`${host}/api/auth/isvalidotp`,{
-        method:'POST',
-        headers:{
-            'Content-Type': 'application/json',
+    const promotoken = localStorage.getItem("promoToken");
+
+    const response = await fetch(`${host}/api/auth/isvalidotp`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
         },
-        body:JSON.stringify({fullname:props.fullname,email:props.email,phone:props.phone,password:props.password,otp:props.otp})
+        body: JSON.stringify({
+            fullname: props.fullname,
+            email: props.email,
+            phone: props.phone,
+            password: props.password,
+            otp: props.otp,
+            promotoken: promotoken,
+        }),
     });
 
     let res = await response.json();
-    return res
-}
+    return res;
+};
 
-export default signin
+export default signin;
